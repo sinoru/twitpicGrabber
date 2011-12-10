67455ecf832c2894611925788107f54b206118ad
Sinoru <sinoru.dev@me.com>
2011-12-11 04:27:41 +0900

    Initial Commit

---
+++ twitpicGrabber/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  twitpicGrabber
+//
+//  Created by 재홍 강 on 11. 12. 11..
+//  Copyright (c) 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
